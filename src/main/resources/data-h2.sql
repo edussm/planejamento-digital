ed90b271ce922309d4388afcc0ed1ee69ff53705
Eduardo Santos <eduardoss@algartelecom.com.br>
2017-09-19 20:17:19 -0300

    scripts do banco para a entidade Roteiro

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -2,3 +2,6 @@
 --Script para popular tabela de atividades
 INSERT INTO atividade (descricao, minutos_necessarios) VALUES ('Correr', 60);
 INSERT INTO atividade (descricao, minutos_necessarios) VALUES ('Nadar', 40);
+
+INSERT INTO roteiro (nome, data_inicio, data_fim) VALUES ('Roteiro I', parsedatetime('19-09-2017 19:00:00', 'dd-MM-yyyy hh:mm:ss'), parsedatetime('19-09-2017 21:00:00', 'dd-MM-yyyy hh:mm:ss'));
+INSERT INTO roteiro (nome, data_inicio, data_fim) VALUES ('Roteiro II', parsedatetime('19-09-2017 21:00:00', 'dd-MM-yyyy hh:mm:ss'), parsedatetime('19-09-2017 22:00:00', 'dd-MM-yyyy hh:mm:ss'));
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -2,6 +2,15 @@ drop table if exists public.atividade;
 
 create table atividade (
 	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	descricao varchar(45),
+	descricao varchar(500) NOT NULL,
 	minutos_necessarios INT
+);
+
+drop table if exists public.roteiro;
+
+create table roteiro (
+	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nome varchar(45) NOT NULL,
+	data_inicio TIMESTAMP,
+	data_fim TIMESTAMP
 );
